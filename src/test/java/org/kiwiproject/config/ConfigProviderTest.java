b520571b786ee6c22786e25056b177b14d8b805f
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-09-11 13:15:10 -0400

    Add base config provider interface (#3)

---
+++ src/test/java/org/kiwiproject/config/ConfigProviderTest.java
@@ -0,0 +1,41 @@
+package org.kiwiproject.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("ConfigProvider")
+class ConfigProviderTest {
+
+    @Nested
+    class CanProvide {
+        @Test
+        void shouldReturnTrueWhenCanProvide() {
+            ConfigProvider provider = () -> true;
+            assertThat(provider.canProvide()).isTrue();
+        }
+
+        @Test
+        void shouldReturnFalseWhenCanNotProvide() {
+            ConfigProvider provider = () -> false;
+            assertThat(provider.canProvide()).isFalse();
+        }
+    }
+
+    @Nested
+    class CanNotProvide {
+        @Test
+        void shouldReturnFalseWhenCanProvide() {
+            ConfigProvider provider = () -> true;
+            assertThat(provider.canNotProvide()).isFalse();
+        }
+
+        @Test
+        void shouldReturnTrueWhenCanNotProvide() {
+            ConfigProvider provider = () -> false;
+            assertThat(provider.canNotProvide()).isTrue();
+        }
+    }
+}
